84fc25ee89559a3c5260460ac51407ed9553a289
Dave Hamilton <djhamilton2@sbcglobal.net>
2016-01-30 22:49:01 -0600

    Started on socket.io channel.

--- src/views/AboutView.js
+++ src/views/AboutView.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router';
+const debug = require('debug')('app:AboutView');
 
 export class AboutView extends React.Component {
+
+  componentDidMount() {
+    this.socket = io.connect('http://localhost:3001/');
+    this.socket.on('connect', () => {
+      debug('connected');
+    });
+    this.socket.on('disconnect', () => {
+      debug('disconnected');
+    });
+    this.socket.on('reconnect', () => {
+      debug('reconnected');
+    });
+    this.socket.on('error', err => {
+      debug('error', err);
+    });
+    this.socket.on('my-message-pong', data => {
+      debug('pong message', data);
+    });
+  }
+
   render () {
+
+    const pingButtonProps = {
+      className: 'btn btn-success',
+      onClick: event => {
+        event.preventDefault();
+        debug('sending ping message');
+        this.socket.emit('my-message-ping', { data: 'about' });
+      }
+    };
+
     return (
       <div className='container text-center'>
         <h1>About</h1>
+        <hr />
+        <button {...pingButtonProps}>Ping</button>
         <hr />
         <Link to='/'>Home</Link>
       </div>
